f47a9bb78a5c5417d98b4ea97df19144430eee77
donebd <dinil.bd@mail.ru>
2022-03-21 20:25:30 +0300

    fix

--- src/test/java/tests/ParentTest.java
+++ src/test/java/tests/ParentTest.java
@@ -7,18 +7,12 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ParentTest {
-    /*
-        Возможно даже необязательно выставлять какие-то настройки, потому что
-        и без них вроде как работало. Не знаю, какой в таком случае используется WebDriver
-     */
+
     final static String USER_PASSWORD = "";
     final static String USER_LOGIN = "";
 
     @BeforeAll
     public static void createWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-        System.setProperty("selenide.browser", "Chrome");
-
         open("https://ok.ru");
     }
 
