b37da1511511ec149348bfec139b96296781f4c8
Денис Жидков <denis.zhidkov01@gmail.com>
2022-04-04 13:53:57 +0300

    Removed my data and configs from commit

--- src/test/java/tests/ParentTest.java
+++ src/test/java/tests/ParentTest.java
@@ -10,14 +10,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class ParentTest {
 
-    final static String USER_LOGIN = "89093832451";
-    final static String USER_PASSWORD = "zx#c93bF7GqHprM";
+    final static String USER_LOGIN = "";
+    final static String USER_PASSWORD = "";
 
     @BeforeEach
     public void createWebDriver() {
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         Configuration.browser = "chrome";
-        Configuration.pageLoadTimeout = 60000;
         open("https://ok.ru");
     }
 
