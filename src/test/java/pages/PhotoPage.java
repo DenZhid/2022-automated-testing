ffb591d789001b9e5c57f8a7933e3ed02052fa67
Денис Жидков <denis.zhidkov01@gmail.com>
2022-04-08 16:39:04 +0300

    Cleanup() in progress + some code cleanup

--- src/test/java/pages/PhotoPage.java
+++ src/test/java/pages/PhotoPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import java.io.File;
+import java.time.Duration;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
@@ -15,20 +16,22 @@ import static com.codeborne.selenide.Selenide.refresh;
 
 public class PhotoPage extends Page {
 
-    private static final SelenideElement PHOTO_TABS_HEADER = $(byXpath("//div[@class='filter photo-vitrine-tabs']"));
-    private static final SelenideElement CREATE_ALBUM_BUTTON = $(byXpath("//a[@data-l='t,create-photo-album']"));
+    private static final ElementsCollection ALL_PHOTOS = $$(byXpath("//img[@class='photo-img__tt8r9']"));
     private static final SelenideElement ALBUM_NAME_EDIT_FIELD = $(byXpath("//textarea[@data-l='t,textField-editor']"));
-    private static final SelenideElement CONFIRM_CREATING_ALBUM_BUTTON = $(byXpath("//input[@data-l='t,confirm']"));
     private static final SelenideElement BACK_TO_ROOT_PHOTO_PAGE_LINK = $(byXpath("//a[@data-l='t,root']"));
-    private static final SelenideElement INPUT_UPLOAD_PHOTO_FIELD =
-            $(byXpath("//span[@data-l='t,upload-new-photo']//input"));
+    private static final SelenideElement CONFIRM_AVATAR_BUTTON = $(byXpath("//button[contains(@class, 'core')]"));
+    private static final SelenideElement CONFIRM_CREATING_ALBUM_BUTTON = $(byXpath("//input[@data-l='t,confirm']"));
+    private static final SelenideElement CREATE_ALBUM_BUTTON = $(byXpath("//a[@data-l='t,create-photo-album']"));
     private static final SelenideElement END_OF_UPLOAD_INDICATOR =
             $(byXpath("//up-panel[@endohwcfl]//div[@end-rcs3rf]"));
-    private static final ElementsCollection ALL_PHOTOS = $$(byXpath("//img[@class='photo-img__tt8r9']"));
+    private static final SelenideElement INPUT_UPLOAD_PHOTO_FIELD =
+            $(byXpath("//span[@data-l='t,upload-new-photo']//input"));
     private static final SelenideElement LAST_PHOTO_LINK = $(byXpath("//div[contains(@class, 'photo-card')]//a"));
+    private static final SelenideElement PHOTO_TABS_HEADER = $(byXpath("//div[@class='filter photo-vitrine-tabs']"));
     private static final SelenideElement SET_AS_AVATAR_BUTTON =
             $(byXpath("//button[contains(@class, 'link')]//span[contains(text(), 'профиля')]"));
-    private static final SelenideElement CONFIRM_AVATAR_BUTTON = $(byXpath("//button[contains(@class, 'core')]"));
+    private static final SelenideElement TO_EDIT_BUTTON =
+            $(byXpath("//a[contains(@class, 'menu-item') and(not(contains(@class, 'primary')))]"));
 
     public PhotoPage() {
         super("Photo page init error", PHOTO_TABS_HEADER);
@@ -36,7 +39,7 @@ public class PhotoPage extends Page {
 
     public PhotoPage createAlbum(String albumName) {
         CREATE_ALBUM_BUTTON.shouldBe(visible).click();
-        ALBUM_NAME_EDIT_FIELD.shouldBe(visible).setValue(albumName);
+        ALBUM_NAME_EDIT_FIELD.shouldBe(visible, Duration.ofSeconds(10)).setValue(albumName);
         CONFIRM_CREATING_ALBUM_BUTTON.shouldBe(visible).click();
         BACK_TO_ROOT_PHOTO_PAGE_LINK.shouldBe(visible).click();
         return this;
@@ -45,7 +48,7 @@ public class PhotoPage extends Page {
     public boolean hasCreatedAlbum(String albumName) {
         return $(byXpath("//div[@class='photo-album-card__cpony']" +
                 "//a[@data-l='t,title']" +
-                "[contains(text(), '"+ albumName +"')]"))
+                "[contains(text(), '" + albumName + "')]"))
                 .shouldBe(visible)
                 .isDisplayed();
     }
@@ -71,5 +74,12 @@ public class PhotoPage extends Page {
     public MainPage goToMain() {
         open("https://ok.ru/");
         return new MainPage();
+    }
+
+    public EditAlbumPage goToEditAlbumPage(String albumName) {
+        $(byXpath("//a[text() = '"+ albumName +"']//../..//div[@data-l='t,show-action-menu']"))
+                .shouldBe(visible).click();
+        TO_EDIT_BUTTON.shouldBe(exist).click();
+        return new EditAlbumPage();
     }
 }
--- src/test/java/tests/PhotoTest.java
+++ src/test/java/tests/PhotoTest.java
@@ -5,11 +5,15 @@ import pages.MainPage;
 import pages.PhotoPage;
 import utils.User;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Selenide.open;
 import static java.io.File.separator;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -20,18 +24,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PhotoTest extends ParentTest {
 
     private static final String ALBUM_NAME = "Test album";
+    private static final String PERSONAL_ALBUM_NAME = "Личные фото";
     private static final String PATH_TO_RESOURCES_FOLDER =
             "src" + separator + "test" + separator + "resources" + separator;
     private static User user;
 
     @BeforeAll
-    public static void createUser() {
+    static void createUser() {
         user = new User(USER_LOGIN, USER_PASSWORD);
     }
 
     // Тест: логинимся -> заходим в раздел "Фото" -> создаём пустой альбом -> проверяем, что альбом создан
     @Test
-    public void createEmptyPhotoAlbumTest() {
+    void createEmptyPhotoAlbumTest() {
         assertTrue(new LogPage()
                 .login(user)
                 .goToPhoto()
@@ -43,7 +48,7 @@ public class PhotoTest extends ParentTest {
     // Тест: логинимся -> загружаем фото -> проверяем, что количество фото пользователя увеличилось
     @ParameterizedTest
     @ValueSource(strings = {"cat.png", "kitty.png"})
-    public void uploadPhotoTest(String photoName) {
+    void uploadPhotoTest(String photoName) {
         PhotoPage photoPage = new LogPage()
                 .login(user)
                 .goToPhoto();
@@ -56,7 +61,7 @@ public class PhotoTest extends ParentTest {
     // Тест: логинимся -> загружаем фото -> ставим фото, как аватар -> проверяем, что ссылка на фото аватара изменилась
     @ParameterizedTest
     @ValueSource(strings = {"cat.png", "kitty.png"})
-    public void changeAvatarTest(String photoName) {
+    void changeAvatarTest(String photoName) {
         MainPage mainPage = new LogPage().login(user);
         String startAvatarRef = mainPage.getCurrentAvatarRef();
         mainPage = mainPage
@@ -66,5 +71,22 @@ public class PhotoTest extends ParentTest {
                 .goToMain();
         String endAvatarRef = mainPage.getCurrentAvatarRef();
         assertThat(startAvatarRef, not(equalTo(endAvatarRef)));
+    }
+
+    // Зачискта после тестов
+    @AfterAll
+    static void cleanup() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        Configuration.browser = "chrome";
+        Configuration.pageLoadStrategy = "eager";
+        open("https://ok.ru");
+        new LogPage()
+                .login(user)
+                .goToPhoto()
+                .goToEditAlbumPage(ALBUM_NAME)
+                .deleteAlbum()
+                .goToEditAlbumPage(PERSONAL_ALBUM_NAME)
+                .deletePhotoByNumber(2)
+                .deletePhotoByNumber(1);
     }
 }
