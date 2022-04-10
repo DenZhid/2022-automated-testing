d4100ff774b8ce64cde08c55432a310ca89385a7
zadorotskas <zadorotskas@yandex.ru>
2022-04-10 15:32:42 +0300

    add music toolbar page element

--- src/test/java/pages/MusicPage.java
+++ src/test/java/pages/MusicPage.java
@@ -9,9 +9,6 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class MusicPage extends Page {
     private static final SelenideElement ARTIST_IMAGE_OVERLAY = $(byXpath("//a[@slot = 'image-overlay']"));
-    private static final SelenideElement SEARCH_INPUT = $(byXpath("//input[@placeholder = 'Поиск']"));
-    private static final SelenideElement SEARCH_SUBMIT_BUTTON
-            = $(byXpath("//*[@data-l='t,suggests']/*[@data-l='t,submit']"));
     private static final SelenideElement MY_MUSIC_BUTTON = $(byXpath("//a[@data-l= 't,library']"));
     private static final SelenideElement ADD_BUTTON
             = $(byXpath("//*[@name='controls']//*[@title='Добавить в мою музыку']"));
@@ -19,12 +16,11 @@ public class MusicPage extends Page {
             = $(byXpath("//*[@name='controls']//*[@data-tsid='remove_track']"));
 
     public MusicPage() {
-        super("Music page init error", PlayButtonFactory.getPlayButton());
+        super("Music page init error", MusicToolbar.getPlayButton());
     }
 
     public MusicPage search(String query) {
-        SEARCH_INPUT.should(visible).setValue(query);
-        SEARCH_SUBMIT_BUTTON.should(visible).click();
+        MusicToolbar.search(query);
         return this;
     }
 
@@ -53,18 +49,33 @@ public class MusicPage extends Page {
         DELETE_BUTTON.should(visible).click();
     }
 
-    private static class PlayButtonFactory {
-        private static final SelenideElement PLAYING_TRACK = $(byXpath("//*[@data-tsid='playing_track']"));
-        private static final SelenideElement PLAY_BUTTON_WITH_PLAYING_TRACK = $(byXpath("//*[@class='play __active']"));
-        private static final SelenideElement PLAY_BUTTON_WITHOUT_PLAYING_TRACK
-                = $(byXpath("//*[@data-tsid='music_player_controls']//*[@data-tsid='play_button']"));
+    private static class MusicToolbar {
+        private static final SelenideElement SEARCH_INPUT = $(byXpath("//input[@placeholder = 'Поиск']"));
+        private static final SelenideElement SEARCH_SUBMIT_BUTTON
+                = $(byXpath("//*[@data-l='t,suggests']/*[@data-l='t,submit']"));
 
         private static SelenideElement getPlayButton() {
-            if (PLAYING_TRACK.exists()) {
-                return PLAY_BUTTON_WITH_PLAYING_TRACK;
-            }
+            return PlayButtonFactory.getPlayButton();
+        }
 
-            return PLAY_BUTTON_WITHOUT_PLAYING_TRACK;
+        private static void search(String query) {
+            SEARCH_INPUT.should(visible).setValue(query);
+            SEARCH_SUBMIT_BUTTON.should(visible).click();
+        }
+
+        private static class PlayButtonFactory {
+            private static final SelenideElement PLAYING_TRACK = $(byXpath("//*[@data-tsid='playing_track']"));
+            private static final SelenideElement PLAY_BUTTON_WITH_PLAYING_TRACK = $(byXpath("//*[@class='play __active']"));
+            private static final SelenideElement PLAY_BUTTON_WITHOUT_PLAYING_TRACK
+                    = $(byXpath("//*[@data-tsid='music_player_controls']//*[@data-tsid='play_button']"));
+
+            private static SelenideElement getPlayButton() {
+                if (PLAYING_TRACK.exists()) {
+                    return PLAY_BUTTON_WITH_PLAYING_TRACK;
+                }
+
+                return PLAY_BUTTON_WITHOUT_PLAYING_TRACK;
+            }
         }
     }
 }
