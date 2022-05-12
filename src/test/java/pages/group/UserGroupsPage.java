b5247d27c22b78517a03882346d243b5e062878e
donebd <dinil.bd@mail.ru>
2022-05-12 22:09:41 +0300

    Must be work??

--- src/test/java/pages/group/UserGroupsPage.java
+++ src/test/java/pages/group/UserGroupsPage.java
@@ -1,8 +1,5 @@
 package pages.group;
 
-import org.openqa.selenium.By;
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
 import pages.BasePage;
@@ -14,16 +11,20 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class UserGroupsPage extends BasePage {
 
-    private final static SelenideElement USER_GROUPS_BLOCK = $(byXpath("//*[@id = 'hook_Block_MyGroupsTopBlock']"));
+    private final static SelenideElement USER_GROUPS_BLOCK = $(byXpath("//*[@id='hook_Block_UserGroupsPanelBlock']"));
     private final static SelenideElement GROUP_SEARCH_INPUT = $(byXpath("//*[@id='hook_Block_UserGroupsCatalogHeaderBlock']//input"));
+
+    private final static SelenideElement GROUP_SEARCH_SUBMIT_BUTTON = $(byXpath("//*[@id='hook_Block_UserGroupsCatalogHeaderBlock']//*[@data-icon-name='ico_search_16']"));
 
     public UserGroupsPage() {
         super("Can't open user groups", $(byId("hook_Block_UserGroupsPanelBlock")));
     }
 
     public GroupPage openGroup(String groupId) {
-        GROUP_SEARCH_INPUT.should(visible).setValue(groupId).pressEnter();
-        USER_GROUPS_BLOCK.$("div[data-group-id='" + groupId + "']").should(visible).click();
+        // Не работает ни пресс ентер ни клик на лупу что то из этого удалить
+        GROUP_SEARCH_INPUT.should(visible).setValue(groupId).pressEnter();// пресс ентер
+//        GROUP_SEARCH_SUBMIT_BUTTON.should(visible).click();// клик на лупу
+        USER_GROUPS_BLOCK.$(byXpath("a[contains (@hrefattrs,'" + groupId + "')]")).should(visible).click();
         return new GroupPage();
     }
 
